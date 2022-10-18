ae20a7a0d105ba8d6a44d39ea1f2cb3122b93b73
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-10-18 22:17:17 +0530

    socket.io

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express')
 const app = express()
 const http = require('http')
 const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
 })
 
 server.listen(3000, () => {

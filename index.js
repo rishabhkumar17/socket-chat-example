d9a65d82c285f08f877b14915fd5f1f5795237ad
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-10-18 23:36:36 +0530

    broadcast emit

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const express = require('express')
-const app = express()
-const http = require('http')
-const server = http.createServer(app)
-const { Server } = require('socket.io')
-const io = new Server(server)
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
+// io.on('connection', (socket) => {
+//   socket.broadcast.emit('hi')
+// })
+
 io.on('connection', (socket) => {
-  console.log('a user connected')
-  socket.on('disconnect', () => {
-    console.log('user disconnected')
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg)
   })
 })
 
-server.listen(3000, () => {
-  console.log('listening on *:3000')
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`)
 })
